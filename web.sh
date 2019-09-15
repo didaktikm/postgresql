14be15215bd98d61c30b117857ad64d734f788ec
Didaktik-M <git@didaktik.com>
2019-09-15 21:47:59 +0300

    add conf

---
+++ web.sh
@@ -0,0 +1,3 @@
+echo "replication"|su - postgres -c "pg_basebackup -h 172.20.10.50 -U replication -D /var/lib/pgsql/11/data/ --wal-method=stream"
+select * from otus;
+INSERT INTO otus VALUES ('test string1945');
