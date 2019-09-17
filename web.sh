69411b69e0a9961f6890e426ea5d418da5ab9ee9
Pavel <slimjim@slimjim.ru>
2019-09-17 18:59:38 +0300

    fix

--- web.sh
+++ web.sh
@@ -1,3 +1,4 @@
 # echo "replication"|su - postgres -c "pg_basebackup -h 172.20.10.50 -U replication -D /var/lib/pgsql/11/data/ --wal-method=stream"
 # select * from otus;
 # INSERT INTO otusdb VALUES ('test string1945');
+# CREATE DATABASE replicatest ENCODING='UTF8';
